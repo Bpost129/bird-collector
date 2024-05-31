aec2ed0f7ca4da46e7fa1a04b5c40b65e9b5455b
Bpost129 <Bpost129@yahoo.com>
2024-05-30 23:47:21 -0400

    complete finch part 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,28 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Add the Cat class & list and view function below the imports
-class Bird:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, description, age):
-    self.name = name
-    self.description = description
-    self.age = age
-
-birds = [
-  Bird('John', 'Kinda rude.', 3),
-  Bird('Ron', 'Looks like a turtle.', 0),
-  Bird('Bob', 'Happy fluff ball.', 4),
-  Bird('Hector', 'Tweets loudly.', 6)
-]
-
+from .models import Bird
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def bird_index(request):
+  birds = Bird.objects.all()
   return render(request, 'birds/index.html', { 'birds': birds })
+
+def bird_detail(request, bird_id):
+  bird = Bird.objects.get(id=bird_id)
+  return render(request, 'birds/detail.html', { 'bird': bird })
